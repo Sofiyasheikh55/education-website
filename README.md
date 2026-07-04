117b3ec7a0c98c843950688392d7a1155ee6bdb9
Sofiya Sheikh <sofiyasheikh555@gmail.com>
2026-07-04 10:22:33 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# EduVerse - Online Learning Platform
+
+## Technologies Used
+- HTML5
+- CSS3
+- Bootstrap 5
+- Vanilla JavaScript (ES6)
+
+## Features
+- Responsive Home Page
+- Popular Courses Section
+- Real-Time Search
+- Category Filter
+- Course Detail Page
+- URL Parameter Based Navigation
+- Enroll Button
+- Responsive Design
+
+## Folder Structure
+/css
+/js
+/image
+index.html
+courses.html
+course-detail.html
+
+## How to Run
+Open index.html in your browser.
---
+++ js/data.js
@@ -0,0 +1,73 @@
+const courses = [
+  {
+   id:1,
+title:"React Development Bootcamp",
+category:"Development",
+price:"₹2,999",
+thumbnail:"image/courses/react.jpeg",
+description:"Master modern React.js by building real-world projects. Learn Components, Hooks, Routing, State Management, REST APIs, and create responsive applications ready for production.",
+instructor:"Rahul Sharma",
+duration:"10 Weeks",
+level:"Intermediate"
+},
+
+{
+id:2,
+title:"JavaScript Mastery",
+category:"Development",
+price:"₹2,499",
+thumbnail:"image/courses/java.jpg",
+description:"Build a strong JavaScript foundation from beginner to advanced. Learn ES6+, DOM Manipulation, Events, APIs, Async Programming, Fetch API and practical coding exercises.",
+instructor:"Amit Verma",
+duration:"8 Weeks",
+level:"Beginner"
+},
+
+{
+id:3,
+title:"UI / UX Design with Figma",
+category:"Design",
+price:"₹1,999",
+thumbnail:"image/courses/ui-ux.jpeg",
+description:"Design attractive and user-friendly interfaces using Figma. Understand design systems, wireframes, prototypes, color theory, typography and responsive layouts.",
+instructor:"Priya Singh",
+duration:"6 Weeks",
+level:"Beginner"
+},
+
+{
+id:4,
+title:"Graphic Design Professional",
+category:"Design",
+price:"₹2,199",
+thumbnail:"image/courses/graphics.jpg",
+description:"Learn Adobe Photoshop, Illustrator and branding principles to create stunning posters, logos, banners and social media creatives for professional projects.",
+instructor:"Neha Kapoor",
+duration:"7 Weeks",
+level:"Intermediate"
+},
+
+{
+id:5,
+title:"Digital Marketing Complete Course",
+category:"Marketing",
+price:"₹2,799",
+thumbnail:"image/courses/D.M.jpg",
+description:"Become a Digital Marketing expert by learning SEO, Social Media Marketing, Google Ads, Email Marketing, Content Strategy and campaign optimization.",
+instructor:"Karan Mehta",
+duration:"9 Weeks",
+level:"Beginner"
+},
+
+{
+id:6,
+title:"SEO Masterclass",
+category:"Marketing",
+price:"₹1,799",
+thumbnail:"image/courses/SEO.jpg",
+description:"Learn advanced SEO strategies including keyword research, technical SEO, on-page optimization, backlinks and website ranking techniques for Google.",
+instructor:"Anjali Gupta",
+duration:"5 Weeks",
+level:"Advanced"
+}
+];
